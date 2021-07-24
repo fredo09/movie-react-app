195f8ac9718937897eee4f0b80ed7e1606975380
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2021-07-24 02:19:53 -0500

    Agregamos el sistema de rutas para la app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
-function App() {
+//Importamos todas las paginas
+import { Home } from './pages/Home';
+import { Movie } from './pages/Movie';
+import { NewMovies } from './pages/New-movies';
+import { PopularMovie } from './pages/Popular-movie';
+import { Search } from './pages/Search';
+import { Error404 } from './pages/Error404';
+
+export default function App() {
+
+  const { Header, Content } = Layout;
+
   return (
-    <div className="App">
-      <h1>🚀  lanzamiento de la app movie-info</h1>
-    </div>
+    <Layout>
+      <Router>
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home/>
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies/>
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Movie/>
+            </Route>
+            <Route path="/popular-movie" exact={true}>
+              <PopularMovie/>
+            </Route>
+            <Route path="/serach" exact={true}>
+              <Search/>
+            </Route>
+
+            <Route path="*">
+              <Error404/>
+            </Route>
+
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 }
-
-export default App;
