9295eeb46173ad1cb1f317b537b307eec4f18cc6
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2021-07-26 01:54:39 -0500

    Se reutilizo el componente de MovieList para mostrar el top de peliculas en la home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,6 +15,10 @@ export const Home = () => {
         `${PATH_API}/movie/popular?api_key=${API_KEY}&language=en-ES&page=1`
     );
 
+    const topMovies = useFetch(
+        `${PATH_API}/movie/top_rated?api_key=${API_KEY}&language=en-ES&page=1`
+    );
+
     return (
         <>
             <SliderComponent movies={newMovies} />
@@ -23,7 +27,7 @@ export const Home = () => {
                     <MovieList title="Peliculas Populares" movies={popularMovies}/>
                 </Col>
                 <Col span={12}>
-                    ...
+                    <MovieList title="Top Peliculas" movies={topMovies}/>
                 </Col>
             </Row>
         </>
