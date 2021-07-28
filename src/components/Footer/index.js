e5e29e2d2b47f53cb16db04c8e39b348e13bb018
Jose Alfredo Vazquez Jimenez <fredy@Air-de-Jose.bwrouter>
2021-07-27 19:56:31 -0500

    Agregando componente footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+/**
+*   Componente Footer 
+**/
+
+import React from 'react';
+
+import './Footer.scss';
+
+export const Footer = () => {
+    return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    )
+}
