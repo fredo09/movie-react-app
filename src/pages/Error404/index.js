423116d2a35828a92523cf7d94beb630e981f4ce
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2021-08-04 01:02:21 -0500

    Se creo en el componente error404

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Footer } from './../../components/Footer';
+import ImgError404 from './../../assets/img/error404.png';
+ 
+import './Error404.scss';
 
 export const Error404 = () => {
     return (
-        <div>
-            <h1>Error404</h1>
-        </div>
+        <>
+            <div className="error404">
+                <div className="error404__img">
+                    <img alt="Error404" src={ImgError404} />
+                </div>
+                <Link to="/">
+                    <p>Ir al inicio</p>
+                </Link>
+            </div>
+            <Footer/>
+        </>
     )
 }
